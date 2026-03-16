e6ad71a83aad501b67f40a9e0ef742084f9e3931
Orest Tryndyak <s32757@pjwstk.edu.pl>
2026-03-16 22:14:22 +0100

    Creating branch feature-max

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
-    
     static double CalculateAverage(int[] values)
     {
         if (values == null || values.Length == 0)
@@ -16,10 +17,32 @@
         return average;
     }
 
+    static int CalculateMax(int[] values)
+    {
+        if (values == null || values.Length == 0)
+            return 0;
+
+        int max = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+
+        return max;
+    }
+
     static void Main()
     {
         int[] numbers = { 5, 10, 15, 20 };
+
         double avg = CalculateAverage(numbers);
+        int max = CalculateMax(numbers);
+
         Console.WriteLine("Average: " + avg);
+        Console.WriteLine("Max: " + max);
     }
 }
